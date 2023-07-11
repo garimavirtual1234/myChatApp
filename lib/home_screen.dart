3fd6ccb977a8bb53bd65a6dc1179ece707969cc4
garimavirtual1234 <garimatyagi@virtualemployee.com>
2023-07-11 11:54:14 +0530

    local

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:chat_bot_demo/chat_screen.dart';
 import 'package:chat_bot_demo/firebase_services.dart';
 import 'package:chat_bot_demo/login_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -31,29 +32,36 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: const Text("logout"))
           ],
         ),
-        body:Container(
-          padding: const EdgeInsets.all(15),
-          child: FutureBuilder(
-            future: FirebaseServices().readUsers(),
-            builder: (context,snapshot) {
+        body:FutureBuilder(
+          future: FirebaseServices().readUsers(),
+          builder: (context,snapshot) {
     if(snapshot.hasData){
     return ListView.builder(
     itemCount: snapshot.data!.docs.length,
     itemBuilder: (BuildContext context,int index){
     DocumentSnapshot products = snapshot.data!.docs[index];
     return Card(
+      elevation: 5,
     child: ListTile(
       onTap: (){
         print("${snapshot.data!.docs[index]}");
         Navigator.push(context, MaterialPageRoute(builder: (context)=>
-          ChatScreen(
-               targetUser: products['id'],
-              // chatRoomModel: null,
-              // firebaseUser: null,
-            )));
+        ChatScreen(
+             targetUser: products['id'],
+            // chatRoomModel: null,
+            // firebaseUser: null,
+          )));
       },
+      leading: const CircleAvatar(
+        child: Icon(CupertinoIcons.person),
+      ),
     title: Text(products['name']),
     subtitle: const Text("last message"),
+      trailing: const Text("12:30 P.M",
+      style: TextStyle(
+        color: Colors.black54
+      ),
+      ),
     ),
     );
     },
@@ -62,9 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
       return const Text("no data present");
     }
     }
-          )
-
-    ));
+        ));
   }
 
 
--- lib/message_model.dart
+++ lib/message_model.dart
@@ -1,14 +1,16 @@
 
 class MessageModel{
   String sender;
+  String receiver;
   String text;
   bool seen;
   DateTime createOn;
 
-  MessageModel({required this.sender, required this.text,required this.seen,required this.createOn});
+  MessageModel({required this.sender,required this.receiver, required this.text,required this.seen,required this.createOn});
 
   Map<String,dynamic> toJson()=>{
     'sender':sender,
+    'receiver':receiver,
     'text':text,
     'seen':seen,
     'createOn':createOn
@@ -17,9 +19,9 @@ class MessageModel{
 
   static MessageModel fromJson(Map<String , dynamic> json) => MessageModel(
       sender: json['sender'],
+      receiver: json['receiver'],
       text: json['text'],
     seen: json['seen'],
     createOn: json['createOn']
-
   );
 }
